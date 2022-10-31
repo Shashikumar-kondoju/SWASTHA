a7b4ca00ae15851e18ab346ac507e8ba3fa36e47
ProjectRelief <wetrivial@gmail.com>
2022-10-31 20:44:55 +0530

    Project_Relief App

--- app/src/main/java/com/androiddev/projectrelief/MainActivity.java
+++ app/src/main/java/com/androiddev/projectrelief/MainActivity.java
@@ -1,14 +1,87 @@
 package com.androiddev.projectrelief;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    NavigationView navigationView;
+    CoordinatorLayout coordinatorLayout;
+    FrameLayout frameLayout;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolBar);
+        navigationView = findViewById(R.id.navigationView);
+        coordinatorLayout = findViewById(R.id.coordinatorLayout);
+        frameLayout = findViewById(R.id.frame);
+        drawerLayout = findViewById(R.id.drawerLayout);
+
+        setUpToolBar();
+        openHome();
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                MainActivity.this,
+                drawerLayout,
+                R.string.open_drawer,
+                R.string.close_drawer
+        );
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        MenuItem prevItemChecked = null;
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+                return false;
+            }
+        });
+
+    }
+    public void openHome(){
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame,new HomeFragment(),"HomeFragment");
+        getSupportFragmentManager().beginTransaction().commit();
+        if(getSupportActionBar()!=null){
+            getSupportActionBar().setTitle("Home");
+        }
+        navigationView.setCheckedItem(R.id.nav_home);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id==android.R.id.home){
+            drawerLayout.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setUpToolBar(){
+        setSupportActionBar(toolbar);
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Title");
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
