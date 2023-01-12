dcd7c4b4aca4fa5c41773018ed87b78f434c8abc
wetrivial <wetrivial@gmail.com>
2023-01-12 16:27:20 +0530

    music player activity

---
+++ app/src/main/java/com/androiddev/projectrelief/activity_musicplayer.kt
@@ -0,0 +1,12 @@
+package com.androiddev.projectrelief
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.androiddev.projectrelief.R
+
+class activity_musicplayer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_musicplayer)
+    }
+}
