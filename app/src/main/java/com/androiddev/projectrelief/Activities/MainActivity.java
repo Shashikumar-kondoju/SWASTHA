2e3a2328ee8f427acdf998328462565451a5d971
ProjectRelief <wetrivial@gmail.com>
2023-01-12 11:42:32 +0530

    logout button added!

--- app/src/main/java/com/androiddev/projectrelief/Activities/MainActivity.java
+++ app/src/main/java/com/androiddev/projectrelief/Activities/MainActivity.java
@@ -4,6 +4,7 @@ import static androidx.constraintlayout.widget.StateSet.TAG;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
@@ -12,6 +13,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -104,16 +106,33 @@ public class MainActivity extends AppCompatActivity {
                     }
                     drawerLayout.closeDrawers();
                 }else if(item.getItemId()==R.id.log_out){
-                    if (FirebaseAuth.getInstance().getCurrentUser() != null)
-                        FirebaseAuth.getInstance().signOut();
-                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    SharedPreferences preferences =getSharedPreferences("login", Context.MODE_PRIVATE);
-                    SharedPreferences.Editor editor = preferences.edit();
-                    editor.clear();
-                    editor.apply();
-                    startActivity(intent);
-                    finishAffinity();
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("Confirm Exit");
+                    builder.setMessage("Are you sure you want to logout?");
+                    builder.setPositiveButton("LOGOUT", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            if (FirebaseAuth.getInstance().getCurrentUser() != null)
+                                FirebaseAuth.getInstance().signOut();
+                            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            SharedPreferences preferences =getSharedPreferences("login", Context.MODE_PRIVATE);
+                            SharedPreferences.Editor editor = preferences.edit();
+                            editor.clear();
+                            editor.apply();
+                            startActivity(intent);
+                            finishAffinity();
+                            dialogInterface.dismiss();
+                        }
+                    });
+                    builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    });
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
                 }
                 return true;
             }
--- app/src/main/java/com/androiddev/projectrelief/Fragments/HealingMusicFragment.java
+++ app/src/main/java/com/androiddev/projectrelief/Fragments/HealingMusicFragment.java
@@ -24,7 +24,6 @@ import com.google.firebase.database.FirebaseDatabase;
  */
 public class HealingMusicFragment extends Fragment {
 
-
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
