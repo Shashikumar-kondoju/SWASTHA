7435d2a0444248ac377441d017189181d0774f98
wetrivial <wetrivial@gmail.com>
2023-02-04 15:16:08 +0530

    IntroActivity.java

---
+++ app/src/main/java/com/androiddev/projectrelief/Activities/IntroActivity.java
@@ -0,0 +1,56 @@
+package com.androiddev.projectrelief.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.androiddev.projectrelief.R;
+
+import agency.tango.materialintroscreen.MaterialIntroActivity;
+import agency.tango.materialintroscreen.SlideFragment;
+import agency.tango.materialintroscreen.SlideFragmentBuilder;
+
+public class IntroActivity extends MaterialIntroActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addSlide(new SlideFragmentBuilder()
+                .title("MENTAL HEALTH")
+                .image(agency.tango.materialintroscreen.R.drawable.ic_next)
+
+                .image(R.drawable.basic)
+                .description("Description 1")
+                .backgroundColor(R.color.backgroundPlayer)
+                .buttonsColor(R.color.colorPrimaryDark)
+
+
+                .build());
+        addSlide(new SlideFragmentBuilder()
+                .title("PHYSICAL HEALTH")
+                .image(agency.tango.materialintroscreen.R.drawable.ic_next)
+                .image(agency.tango.materialintroscreen.R.drawable.abc_ic_ab_back_material)
+                .image(R.drawable.exercises)
+                .description("Description 2")
+                .backgroundColor(R.color.backgroundPlayer)
+                .buttonsColor(R.color.colorPrimaryDark)
+
+
+                .build());
+        addSlide(new SlideFragmentBuilder()
+                .title("HEALING MUSIC")
+                .image(agency.tango.materialintroscreen.R.drawable.ic_finish)
+                .image(agency.tango.materialintroscreen.R.drawable.abc_ic_ab_back_material)
+                .image(R.drawable.music)
+                .description("Description 3")
+                .backgroundColor(R.color.backgroundPlayer)
+                .buttonsColor(R.color.colorPrimaryDark)
+                .build());
+
+    }
+
+}
